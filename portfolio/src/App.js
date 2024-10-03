45ab3667de404652a374bcb72760791d898ddbd4
Kushagra Dwivedi <kishudwivedi30@gmail.com>
2024-10-03 23:09:53 +0530

    updated

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 import Navbar from './Navbar';
 import './App.css';
 import Hero from './Hero';
+import Page2 from './Page2';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Hero />
+      <Page2 />
     </div>
   );
 }
--- portfolio/src/Page2.js
+++ portfolio/src/Page2.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Page2.css";
-import resumephoto from "../src/assets/images/resumephoto1.jpg"
+import resumephoto from "../src/assets/images/photo.jpg";
 
 
-const Section2 = () => {
+const Page2 = () => {
     return(
         <div className="pagemainsection">
             <div className="photo">
@@ -11,10 +11,14 @@ const Section2 = () => {
             </div>
             <div className="aboutme">
             <h1 className="photo-title">ABOUT ME</h1>
-            <p className="hero-description">A freelancing front end developer & graphic and web</p>
-            <p className="hero-description1">designer, with a love for designing. I am currently</p>
-            <p className="hero-description1">open and working for clients.</p>
+            <p className="about-description">Currently, I am a student at VIT Bhopal, India. Pursuing an integrated masters in Cybersecurity. Whilst, I have an interest for what I am studying, I have a passion for designing and development and have previously freelanced as a graphic and ui/ux designer.</p>
+            <div className="callme">
+                <p className="whiteme">Want to work together?</p>
+                <a href="contact" className="orangeme">Drop me a line.</a>
+            </div>
             </div>
         </div>
     );
 }
+
+export default Page2;
